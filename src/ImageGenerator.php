cbdcd885eeb762e98872f689d1c74fe540b4f42a
Волочаев Даниил <volochaev@techart.ru>
2022-09-07 17:08:09 +0300

    Добавлена очистка папки, создание папки

--- src/ImageGenerator.php
+++ src/ImageGenerator.php
@@ -5,7 +5,6 @@ namespace Volochaev\ImageGeneration;
 use Volochaev\ImageGeneration\Image;
 class ImageGenerator 
 {
-
 	protected const COLORS = [
 		'#FFFFFF',
 		'#808080',
@@ -20,16 +19,32 @@ class ImageGenerator
 		'#8b0000',
 	];
 
+	protected $amount = 30;
+	protected $test;
 
 	public function __construct()
 	{
-		$now = microtime(true);
-		for ($i = 0; $i < 10; $i++) {
+		$this->createDir();
+		$this->clear();
+		for ($i = 0; $i < $this->amount / 3; $i++) {
 			$image = new Image(512, 512, $this->getRandomColor());
+			$image->addFigures();
+			$image->drawFigures();
+			$image->addPivots();
 			$image->generateAndSave();
 		}
-		for ($i = 0; $i < 10; $i++) {
+		for ($i = 0; $i < $this->amount / 3; $i++) {
 			$image = new Image(768, 768, $this->getRandomColor());
+			$image->addFigures();
+			$image->drawFigures();
+			$image->addPivots();
+			$image->generateAndSave();
+		}
+		for ($i = 0; $i < $this->amount / 3; $i++) {
+			$image = new Image(1024, 1024, $this->getRandomColor());
+			$image->addFigures();
+			$image->drawFigures();
+			$image->addPivots();
 			$image->generateAndSave();
 		}
 	}
@@ -39,5 +54,22 @@ class ImageGenerator
 		$colors = static::COLORS;
 		$colorIndex = mt_rand(0,count($colors) - 1);
 		return $colors[$colorIndex];
+	}
+
+	private function clear() {
+		$dir = __DIR__ . "/../dataset/";
+		$objects = scandir(__DIR__ . "/../dataset/");
+		foreach($objects as $object) {
+			if ($object != '.' && $object !== '..' && $object !== '.gitignore') {
+				unlink($dir . $object);
+			}
+		}
+	}
+
+	private function createDir() {
+		if (is_dir(__DIR__ . "/../dataset")) {
+			return;
+		}
+		mkdir(__DIR__ . '/../dataset');
 	}
 }
